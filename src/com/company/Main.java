6508080083fb5903ebf2619c69dfb48b4cd783e3
natalia <natalia@Users-Mac-mini.local>
2019-04-24 14:44:29 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int a=10;
+        int b=12;
+        //Zadanie ze szkolenia:
+        Wylicz w=new Wylicz(10,12);
+        w.dzielnik();
+
+    }
+
+}
---
+++ src/com/company/Wylicz.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Wylicz {
+    int a;
+    int b;
+
+    public Wylicz(int a,int b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+
+
+    public int dzielnik()
+    {
+        int wynik = 1;
+        Boolean bool=false;
+        for (int i=a;i>0;i--)
+        {
+            if (a%i==0)
+            {
+                if (b%i==0)
+                {
+                    bool=true;
+                    System.out.println("Najwiekszy wspolny dzielnik: "+i);
+                    i=0;
+                }
+            }
+        }
+        if (bool==false)
+            System.out.println("Najwiekszy wspolny dzielnik to 1");
+        return wynik;
+
+    }
+
+}
