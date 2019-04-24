cf0b59f2c4937f7ea172597c51260f0e6eb91dd4
nataliagilewska <n.gilewska@tt.com.pl>
2019-04-24 15:37:05 +0200

    2 zadanie ze szkolonka

--- src/com/company/Dzialania.java
+++ src/com/company/Dzialania.java
@@ -2,10 +2,18 @@ package com.company;
 
 public class Dzialania {
     int[]tab;
+    int zwiekszO=7;
+    String imie;
 
     public Dzialania(int[] tab)
     {
         this.tab=tab;
+    }
+
+    public Dzialania(int[] tab, int zwiekszO)
+    {
+        this.tab=tab;
+        this.zwiekszO=zwiekszO;
     }
 
     public int suma()
@@ -18,10 +26,22 @@ public class Dzialania {
         {
             wynik=wynik+tab[i];
         }
-
         return wynik;
-
     }
 
+    //public int[] dodaj(int[] tablica, int zwiekszOliczbe)
+    public int[] dodaj()
+    {
+        int wynik=0;
+        int pom=tab.length;
+        int i;
+
+        for (i=0;i<pom;i++)
+        {
+            System.out.println(imie);
+            tab[i]+=zwiekszO;
+        }
+        return tab;
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,9 +14,16 @@ public class Main {
         int[] tab = {1,2,4,5,8};
         //tab.for <- jak to wpisze to mam szybka podpowiedz fora
         //String imie="Michal";
-        //if(imie.equals("Michal")) - dla porownywania stringow lepiej uzywac equals
+        //if(imie.equals("Michal")) - dla porownywania stringow ZAWSZE UZYWAC EQUALS!!! (bo zwykle porownanie string1==string2 porowna miejsca pamieci)
         Dzialania obiekt = new Dzialania(tab);
         System.out.println("Suma elementow tablicy: " + obiekt.suma());
+        //3 zad ze szkolenia
+        int []tab2 =obiekt.dodaj();
+        for (int j=0;j<tab2.length;j++)
+        {
+            System.out.print(tab2[j]+" ");
+
+        }
     }
 
 }
